a85130d4eb6d47c5237444836b779f17646f0688
Pr1nkos <prinkos1@gmail.com>
2025-11-01 16:15:50 +0300

    add(scope): dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,19 +10,30 @@ repositories {
     mavenCentral()
 }
 val seleniumVer = "4.38.0"
-val jacksonVer = "3.0.1"
+val jacksonVer = "2.20.1"
 val opencsvVer = "5.12.0"
+val jsoupVer = "1.21.2"
+val slf4jVer = "2.0.17"
+val kotLoggingVer = "7.0.3"
+val logbackVer = "1.5.20"
 
 dependencies {
     testImplementation(kotlin("test"))
     // https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java
     implementation("org.seleniumhq.selenium:selenium-java:${seleniumVer}")
     // https://mvnrepository.com/artifact/tools.jackson.dataformat/jackson-dataformat-yaml
-    implementation("tools.jackson.dataformat:jackson-dataformat-yaml:${jacksonVer}")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:${jacksonVer}")
     // https://mvnrepository.com/artifact/tools.jackson.module/jackson-module-kotlin
-    implementation("tools.jackson.module:jackson-module-kotlin:${jacksonVer}")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVer}")
     // https://mvnrepository.com/artifact/com.opencsv/opencsv
     implementation("com.opencsv:opencsv:${opencsvVer}")
+    // https://mvnrepository.com/artifact/org.jsoup/jsoup
+    implementation("org.jsoup:jsoup:${jsoupVer}")
+// https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:${slf4jVer}")
+    implementation("io.github.oshai:kotlin-logging-jvm:${kotLoggingVer}")
+    // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
+    implementation("ch.qos.logback:logback-classic:${logbackVer}")
 }
 
 tasks.test {
